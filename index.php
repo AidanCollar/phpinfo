035ccae96b3732cb5e5dcea7dfdb6af40ad2a26f
AidanCollar <300088221@fvtc.edu>
2024-01-24 19:54:20 -0600

    init

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Aidan's Page!</title>
+    <link rel="stylesheet" href="css/base.css">
+</head>
+<body>
+<header>
+    <nav>
+        <ul>
+            <li>
+                <a href="/">Homepage</a>
+            </li>
+            <li>
+                <a href="#">LoopDemo</a>
+            </li>
+            <li>
+                <a href="#">CountDown</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+<main>
+    <img src="images/Monkey-with-Money.jpg" alt="MONKEY WITH MONEY">
+    <p>
+        Somebody once told me that I was built like a monkey and that is not true. Monkeys don't have the mental capacity that I have. Not only that, I don't eat bananas like monkeys do. I would appreciate if whoever started this rumor would come forward and get slapped. Anyway Ive been making money like this monkey and there is no-one to stop me except myself.
+    </p>
+</main>
+<footer>
+    &copy; <?= date("Y") ?> Aidan Collar
+</footer>
+</body>
+</html>
